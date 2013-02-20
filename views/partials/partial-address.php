75a75aa7ea4250dc6c1423255a4954c15080c685
Colin Rotherham <work@colinr.com>
2013-02-20 09:25:23 +0000

    Sample address partial should output HTML entities

--- views/partials/partial-address.php
+++ views/partials/partial-address.php
@@ -4,11 +4,11 @@
 	$resources = $this->resources;
 
 	// Resource example
-	$address_1 = $resources->get('Contact Us', 'Address Line 1');
-	$address_2 = $resources->get('Contact Us', 'Address Line 2');
-	$town = $resources->get('Contact Us', 'Town');
-	$county = $resources->get('Contact Us', 'County');
-	$postcode = $resources->get('Contact Us', 'Postcode');
+	$address_1 = $resources->html('Contact Us', 'Address Line 1');
+	$address_2 = $resources->html('Contact Us', 'Address Line 2');
+	$town = $resources->html('Contact Us', 'Town');
+	$county = $resources->html('Contact Us', 'County');
+	$postcode = $resources->html('Contact Us', 'Postcode');
 ?>
 			<div class="address">
 <?php
