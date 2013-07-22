bb446da24f57034c860757ad260074b7065f77ea
Colin Rotherham <work@colinr.com>
2013-07-22 17:37:20 +0100

    Wire up 404 page with new $router->path method for home link

--- views/error-404.php
+++ views/error-404.php
@@ -1,6 +1,7 @@
 <?php
 	$template = $view->template;
 	$resources = $template->resources;
+	$router = $view->app->router;
 
 	$template->title = 'Sorry, page not found';
 
@@ -9,7 +10,7 @@
 ?>
 			<h1><?= $resources->html('404', 'Heading') ?></h1>
 			<p><?= $resources->html('404', 'Intro') ?></p>
-			<p><?= $resources->html('Shared', 'Go to') ?> <a href="/"><?= $resources->html('Home', 'Heading') ?></a></p>
+			<p><?= $resources->html('Shared', 'Go to') ?> <a href="<?= $router->path('home') ?>"><?= $resources->html('Home', 'Heading') ?></a></p>
 <?php
 	// End placeholder
 	$template->placeHolderEnd();
