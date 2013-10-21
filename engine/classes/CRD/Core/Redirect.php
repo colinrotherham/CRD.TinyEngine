14b4ca6bc0408332ece4c5abcc06f846e9bde84a
Colin Rotherham <work@colinr.com>
2013-10-21 10:09:21 +0100

    Override query and hash fragment components when redirecting

--- engine/classes/CRD/Core/Redirect.php
+++ engine/classes/CRD/Core/Redirect.php
@@ -12,52 +12,78 @@
 		private static $permanent = 301;
 		private static $temporary = 302;
 
-		public static function to($url, $is_permanent = false, $params = array())
+		private static $components = array();
+
+		// Redirect to a URL, optionally override URL components
+		public static function to($url, $is_permanent = false, $override = array())
 		{
 			// Which status code?
 			$status = ($is_permanent)? self::$permanent : self::$temporary;
-			$components = parse_url($url);
 
-			// Add query string params
-			if (!empty($params))
-			{
-				if (!empty($components['query']))
-				{
-					// Extract current query
-					$query = explode('&', html_entity_decode($components['query']));
+			// Split out URL components
+			self::$components = self::parse($url, $override);
 
-					// Add/Replace to $params
-					foreach($query as $pair)
-					{
-						$pair = explode('=', $pair);
+			// Perform redirect
+			header('Location: ' . self::build(), true, $status);
+			exit;
+		}
 
-						if (!array_key_exists($pair[0], $params))
-							$params[$pair[0]] = $pair[1];
-					}
-				}
-
-				// New combined/replaced query string
-				$components['query'] = http_build_query($params);
-			}
-
+		public static function build()
+		{
+			// Build URL
 			$url = '';
 
 			// Start with a scheme
-			if (!empty($components['host']))
-				$url .= (!empty($components['scheme'])? $components['scheme'] : 'http') . '://' . $components['host'];
+			if (!empty(self::$components['host']))
+				$url .= (!empty(self::$components['scheme'])? self::$components['scheme'] : 'http') . '://' . self::$components['host'];
 
-			if (!empty($components['path']))
-				$url .= $components['path'];
+			if (!empty(self::$components['path']))
+				$url .= self::$components['path'];
 
-			if (!empty($components['query']))
-				$url .= '?' . $components['query'];
+			if (!empty(self::$components['query']))
+				$url .= '?' . self::$components['query'];
 
-			if (!empty($components['fragment']))
-				$url .= '#' . $components['fragment'];
+			if (!empty(self::$components['fragment']))
+				$url .= '#' . self::$components['fragment'];
 
-			// Perform redirect
-			header('Location: ' . $url, true, $status);
-			exit;
+			return $url;
+		}
+
+		public static function parse($url, $override)
+		{
+			self::$components = parse_url($url);
+
+			if (is_array($override))
+			{
+				// Override query string params
+				if (!empty($override['query']))
+				{
+					// Combine with existing params
+					if (!empty(self::$components['query']))
+					{
+						// Extract current query
+						$query = explode('&', html_entity_decode(self::$components['query']));
+
+						// Add/Replace to $override
+						foreach($query as $pair)
+						{
+							$pair = explode('=', $pair);
+
+							if (!array_key_exists($pair[0], $override))
+								$override['query'][$pair[0]] = $pair[1];
+						}
+					}
+
+					// New combined/replaced query string
+					self::$components['query'] = http_build_query($override['query']);
+				}
+
+				// Override hash fragment
+				if (!empty($override['fragment']))
+					self::$components['fragment'] = $override['fragment'];
+			}
+
+			return self::$components;
 		}
 	}
 ?>
